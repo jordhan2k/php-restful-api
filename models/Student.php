a70d92662b8e61ebc63a1686f3c5385425647587
Jordan Han <dunghoang1c.hanu@gmail.com>
2021-03-04 23:15:32 +0700

    Update pdo

--- models/Student.php
+++ models/Student.php
@@ -39,6 +39,7 @@ class Student{
 		return $ps;
 	}
 
+	// GET
 	public function readSingleStudent(){
 
 		$query = 'SELECT * FROM '. $this->table . ' WHERE id = ? LIMIT 0, 1';
@@ -60,6 +61,8 @@ class Student{
 		$this->gender = $row['gender'];
 	}
 
+
+	// POST
 	public function createStudent(){
 		$query = 'INSERT INTO '. $this->table. 
 		' SET
@@ -95,6 +98,68 @@ class Student{
 			printf('Error: %s.\n', $ps->error);
 			return false;
 		}
+	}
+
+	// PUT
+	public function updateStudent(){
+		$query = 'UPDATE '. $this->table. 
+		' SET
+			firstName =:firstName,
+			lastName =:lastName,
+			dob =:dob,
+			address =:address,
+			gender =:gender
+		WHERE id =:id
+			';
+
+		// preparedStatement
+		$ps = $this->conn->prepare($query);
+
+		// clean data 
+		// use htmlspecialchars
+		$this->firstName = htmlspecialchars(strip_tags($this->firstName));
+		$this->lastName = htmlspecialchars(strip_tags($this->lastName));
+		$this->dob = htmlspecialchars(strip_tags($this->dob));
+		$this->address = htmlspecialchars(strip_tags($this->address));
+		$this->gender = htmlspecialchars(strip_tags($this->gender));
+		$this->id = htmlspecialchars(strip_tags($this->id));
+	
+		// bind parameters
+		$ps->bindParam(':firstName', $this->firstName);
+		$ps->bindParam(':lastName', $this->lastName);
+		$ps->bindParam(':dob', $this->dob);
+		$ps->bindParam(':address', $this->address);
+		$ps->bindParam(':gender', $this->gender);
+		$ps->bindParam(':id', $this->id);
+
+		// execute query
+		if ($ps->execute()){
+			return true;
+		} else {
+			// print error
+			printf('Error: %s.\n', $ps->error);
+			return false;
+		}
+	}
+
+
+	public function deleteStudent(){
+		$query = 'DELETE FROM '.$this->table.' WHERE id=:id';
+
+		$ps = $this->conn->prepare($query);
+
+		$this->id = htmlspecialchars(strip_tags($this->id));
+
+		$ps->bindParam(':id', $this->id);
+
+		if ($ps->execute()){
+			return true;
+		} else {
+			// print error
+			printf('Error: %s.\n', $ps->error);
+			return false;
+		}
+
 	}
 
 }
